d943d489a5e29abe16061e15d57a81cdf45d71b1
Hongbo Zhang <bobzhang1988@gmail.com>
2017-12-21 10:13:00 +0800

    init

---
+++ README.md
@@ -0,0 +1,24 @@
+run `ocaml gen.ml` to generate a performance test for bsb
+
+The test involves the generation of DR * DC directories, the
+"directory matrix", and each directory contains MR * MC modules,
+the "module matrix". The module in row r and column c of the module
+matrix depends on all modules in the previous row of the same
+directory. The first row of modules in a directory depends on 
+all modules in the preceding row of directories.
+
+
+The test setup also permits a lot of parallelism for actually executing
+the rules: the modules in the same row can be compiled in parallel,
+as well as the directories in the same row.
+
+Every module includes a big comment, so that the size of the files
+is not super-small. 
+
+To test it 
+
+```sh
+ocaml unix.cma gen.ml -n 2 test && cd test && time bsb
+```
+
+Note to test raw performance, it is recommended to use `bsb.exe`
---
+++ gen.ml
@@ -0,0 +1,99 @@
+open Printf
+
+let dir_rows = ref 1
+let dir_cols = ref 1
+let mod_rows = ref 1
+let mod_cols = ref 1
+let comment_size = ref 5000
+
+
+let count n =
+  Array.to_list (Array.init n (fun k -> k+1))
+
+
+let write_directory basedir dir_row dir_col =
+  let dirname = sprintf "%s/dir_%d_%d" basedir dir_row dir_col in
+  Unix.mkdir dirname 0o777;
+
+  for row = 1 to !mod_rows do
+    for col = 1 to !mod_cols do
+      let deps =
+        if row = 1 then
+          if dir_row = 1 then
+            []
+          else
+            List.flatten
+              (List.map 
+                 (fun k -> 
+                    List.map
+                      (fun j ->
+                         sprintf "M_%d_%d_%d_%d.f()" (dir_row-1) j !mod_rows k
+                      )
+                      (count !dir_cols)
+                 )
+                 (count !mod_cols)
+              )
+        else
+          List.map 
+            (fun k -> sprintf "M_%d_%d_%d_%d.f()" dir_row dir_col (row-1) k)
+            (count !mod_cols) in
+
+      let deps =
+        List.rev ("()" :: (List.rev deps)) in
+
+      let str_deps = String.concat ";\n  " deps in
+      let comment = String.make !comment_size 'X' in
+      let mod_text = sprintf "(* %s *)
+let f() =
+  %s
+" 
+          comment
+          str_deps in
+      let f = open_out
+          (sprintf "%s/m_%d_%d_%d_%d.ml" dirname
+             dir_row dir_col row col) in
+      output_string f mod_text;
+      close_out f
+    done
+  done
+let bsconfig = {|
+{
+    "name": "test",
+    "sources" : {
+        "dir": "src",
+        "subdirs" : true
+    }
+}
+|}
+let write basedir =
+  let () = Unix.mkdir basedir 0o777 in 
+  let f = open_out (Filename.concat basedir "bsconfig.json") in 
+  output_string f bsconfig ; 
+  let () = close_out f in 
+  let basedir = (Filename.concat basedir "src") in
+  let () = Unix.mkdir basedir 0o777 in 
+  for row = 1 to !dir_rows do
+    for col = 1 to !dir_cols do
+      write_directory basedir row col
+    done
+  done
+
+let () =
+  let basedir = ref "." in
+  Arg.parse
+    [ 
+      "-n", Arg.Int (fun n -> dir_rows := n;
+                      dir_cols := n;
+                      mod_rows := n;
+                      mod_cols := n),
+      "<n>  set all of -dir-rows, -dir-cols, -mod-rows, -mod-cols to the same value";
+
+      "-comment-size", Arg.Set_int comment_size,
+      "<n>  size of the module comment";
+    ]
+    (fun d ->
+       basedir := d
+    )
+    (sprintf "usage: %s [basedir]" (Filename.basename Sys.argv.(0)));
+
+  write !basedir
